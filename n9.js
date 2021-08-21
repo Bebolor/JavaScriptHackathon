30324f03891f18fb380838d91226ee3cd4cd8d4c
Bebolor <bolorchimeg.m@gmail.com>
2021-08-21 18:56:58 +0200

    daily js challenge

--- n9.js
+++ n9.js
@@ -1,13 +1,6 @@
-/*9. Merge
-merge takes two or more objects and returns one object with all the properties of the objects passed. If a property is in more than one object, the object passed first should have priority.
+//9. Merge
 
-Example:
-
-merge({ a: 3, b: 2 }, { a: 2, c: 4 }); // { a: 3, b: 2, c: 4 }
-merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5}); // { a: 3, b: 2, c: 4, e: 8 }
-*/
-
-/*const merge = (...objects) =>{
+const merge = (...objects) =>{
     let returnObject = {};
     objects.forEach(object => {
         for(key in object){
@@ -20,4 +13,4 @@ merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5}); // { a: 3, b: 2, c: 4, e: 
 }
 
 console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 }, { e: 8, c: 5})); // { a: 3, b: 2, c: 4, e: 8 } 
-console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 })); // { a: 3, b: 2, c: 4 }*/
+console.log(merge({ a: 3, b: 2 }, { a: 2, c: 4 })); // { a: 3, b: 2, c: 4 }
