c3aee054998d5b8e9bba6fc1c252093ce4afe58e
Bebolor <bolorchimeg.m@gmail.com>
2021-08-24 20:16:46 +0200

    daily js challenge

---
+++ n15.js
@@ -0,0 +1,9 @@
+//15. Print the Century
+
+function century(year) {
+    return Math.floor((year-1)/100) + 1;
+}
+
+console.log(century(999)); // "10th century"
+console.log(century(1001)); // "11th century"
+console.log(century(2005)); // "21st century"
