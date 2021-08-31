fbc288e4a0dc45d8c561ce657fd2d6f7240d3bd2
Bebolor <bolorchimeg.m@gmail.com>
2021-08-31 19:15:21 +0200

    daily js challenge

--- n24.js
+++ n24.js
@@ -4,7 +4,7 @@ const smallest = (num) => {
     const result = num.toString().split('').map((element, index) => [parseInt(element), index] ).filter(element => element[0] === minimum);
     return result.length <= 1 ? result[0] : result;
 };
-console.log(smallest(261235)); 
-console.log(smallest(5967392));
-console.log(smallest(517418)); 
-console.log(smallest(467498));  
+console.log(smallest(261235)); // [ 1, 2 ]
+console.log(smallest(5967392)); // [ 2, 6 ]
+console.log(smallest(517418)); // [ [ 1, 1 ], [ 1, 4 ] ]
+console.log(smallest(467498)); // [ [ 4, 0 ], [ 4, 3 ] ] 
