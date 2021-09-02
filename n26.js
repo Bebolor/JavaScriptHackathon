3e723a42b7ec02d0bad702502a6059d7a5daca1b
Bebolor <bolorchimeg.m@gmail.com>
2021-09-02 22:03:34 +0200

    daily js challenge

--- n26.js
+++ n26.js
@@ -1,6 +1,6 @@
 // 26. … then create Camel Case
 const camelizeIt = string => {
-let CSL= string.split(/([A-Z])/g).map((word)charAt(0).toUpperCase()+ word.söice(1).join("")); 
+let CSL= string.split(/([A-Z])/g).map((word), charAt(0).toUpperCase()+ word.söice(1).join(" ")); 
 return camel.charAt(0).toLowerCase() + camel.slice(1);}
 
-console.log(camelizeIt("Hello World"))}; 
+console.log(camelizeIt("Hello World")); 
