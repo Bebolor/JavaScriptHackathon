db163837f696274b2687197dd1427b0bd601d1e2
Bebolor <bolorchimeg.m@gmail.com>
2021-09-22 22:46:12 +0200

    daily js challenge

--- n35.js
+++ n35.js
@@ -1,9 +1,14 @@
 //35. Is it IPv4 ?
+const checkIP = ip => {
+    let rx=/^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/;
+    return rx.test(ip);
+}
 
-checkIP("127.0.0.1") // true
 
-checkIP("255.255.255.255") // true
+console.log(checkIP("127.0.0.1")) // true
 
-checkIP("123.023.400.011") // false, cannot go further than 255
+console.log(checkIP("255.255.255.255")) // true
 
-checkIP("123.090.020.011") // false, cannot start with a 0
+console.log(checkIP("123.023.400.011")) // false
+
+console.log(checkIP("123.090.020.011")) // false
