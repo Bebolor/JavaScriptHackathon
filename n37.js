f6b60c54bbdf01fc4e50bd3314e31cc75e4fcb99
Bebolor <bolorchimeg.m@gmail.com>
2021-09-29 20:37:25 +0200

    daily js challenge

--- n37.js
+++ n37.js
@@ -27,6 +27,6 @@ const tuner = currentFrequencies => {
 
 console.log(tuner([0, 246.94, 0, 0, 0, 80])) // ["X", "Good", "X", "X", "X", "++"]
 
-tuner([329, 246, 195, 146, 111, 82]) // ["Good", "Good", "+", "+", "-", "Good" ]
+console.log(tuner([329, 246, 195, 146, 111, 82])) // ["Good", "Good", "+", "+", "-", "Good" ]
 
-tuner([329.63, 246.94, 196, 146.83, 110, 82.41]) // ["Good", "Good", "Good", "Good", "Good", "Good"]
+console.log(tuner([329.63, 246.94, 196, 146.83, 110, 82.41])) // ["Good", "Good", "Good", "Good", "Good", "Good"]
