c64fd4c888be9091e9823d185d94da3cbe0f77b4
Bebolor <bolorchimeg.m@gmail.com>
2021-09-29 20:36:45 +0200

    daily js challenge

--- n37.js
+++ n37.js
@@ -1,5 +1,31 @@
 //37. Help Jimi Hendrix
-tuner([0, 246.94, 0, 0, 0, 80]) // ["X", "Good", "X", "X", "X", "++"]
+const tuner = currentFrequencies => {
+    let correctFrequencies = [329.63,246.94,196.00,146.83,110.00,82.41];
+    let currentFrequenciesMetricsOriginal = currentFrequencies.map((frequency,index)=>frequency/correctFrequencies[index]);
+    let currentFrequenciesMetrics = currentFrequenciesMetricsOriginal.map(originalFrequency => Math.round(originalFrequency * 100) / 100);
+    let results = [];
+    currentFrequenciesMetrics.forEach(metrics => {
+        if(metrics === 0){
+                results.push("X");
+        }else if(metrics>0.99 && metrics<1.01){
+                results.push("Good");
+        }else if(metrics<=0.99 && metrics>0.98){ 
+                results.push("+");            
+        }else if(metrics>=1.01 && metrics<1.02){ 
+            results.push("-");  
+        }else{
+            if(metrics<=0.98){
+                results.push("++");
+            }else{
+                results.push("--");
+            }
+        }              
+    });
+    return results; 
+}
+
+
+console.log(tuner([0, 246.94, 0, 0, 0, 80])) // ["X", "Good", "X", "X", "X", "++"]
 
 tuner([329, 246, 195, 146, 111, 82]) // ["Good", "Good", "+", "+", "-", "Good" ]
 
