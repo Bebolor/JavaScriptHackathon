0202d2338ce758c4b1b28b8292311be91ce1b412
Bebolor <bolorchimeg.m@gmail.com>
2021-10-12 21:07:50 +0200

    daily js challenge

--- n38.js
+++ n38.js
@@ -1,7 +1,23 @@
 // 38. Get all the pairs
-allPairs([2, 4, 5, 3], 7) // [[2, 5], [3, 4]]
+
+const allPairs = (args, sum) => {
+    let result = [];
+    for (let i = 0; i < args.length; i++)
+    {
+        for (let j = i; j < args.length; j++)
+        {
+            if (args[i] + args[j] === sum) {
+                if (args[i] < args[j]) result.push([args[i], args[j]]);
+                else result.push([args[j], args[i]]);
+            };
+        }
+    }
+    return result.sort((a, b) => a[0] - b[0]);
+}
+
+console.log(allPairs([2, 4, 5, 3], 7)) // [[2, 5], [3, 4]]
 // 2 + 5 = 7 and 3 + 4 = 7
 
-allPairs([5, 3, 9, 2, 1], 3) // [[1, 2]]
+console.log(allPairs([5, 3, 9, 2, 1], 3)) // [[1, 2]]
 
-allPairs([4, 5, 1, 3, 6, 8], 9) // [[1, 8], [3, 6], [4, 5]]
+console.log(allPairs([4, 5, 1, 3, 6, 8], 9)) // [[1, 8], [3, 6], [4, 5]]
