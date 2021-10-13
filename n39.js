36be006dc12fac521271f9c0944d7e6c27a54547
Bebolor <bolorchimeg.m@gmail.com>
2021-10-13 23:54:03 +0200

    daily js challenge

--- n39.js
+++ n39.js
@@ -1,8 +1,33 @@
 //39. Don’t worry, be happy ?
-happyOrNot(19) // true
+
+const happyOrNot = (number) => {
+    const array = number.toString().split('').map(x => parseInt(x));
+    let sum = 0;
+    let result = false;
+    const total = array.forEach(x => {
+        return sum += Math.pow(x, 2);
+    });
+    if (sum >= 10) {
+        result = happyOrNot(sum);
+        if (result) {
+            return true;
+        } else {
+            return false;
+        }
+    } else {
+        if (sum === 1) {
+            result = true;
+            return true;
+        } else {
+            return false;
+        }
+    }
+};
+
+console.log(happyOrNot(19)) // true
 // because 19 >> 1^2 + 9^2 = 82 >> 8^2 + 2^2 = 68 >> 6^2 + 8^2 = 100 >> 1^2 + 0^2 + 0^2 = 1
 // We reached 1 meaning that 19 is a Happy Number.
 
-happyOrNot(203) // true
-happyOrNot(11) // false
-happyOrNot(107) // false
+console.log(happyOrNot(203)) // true
+console.log(happyOrNot(11)) // false
+console.log(happyOrNot(107)) // false
