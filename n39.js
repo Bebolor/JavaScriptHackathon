3a54e1518cd9976bddeffcde125d0df6ea73f609
Bebolor <bolorchimeg.m@gmail.com>
2021-10-13 23:49:26 +0200

    daily js challenge

---
+++ n39.js
@@ -0,0 +1,8 @@
+//39. Don’t worry, be happy ?
+happyOrNot(19) // true
+// because 19 >> 1^2 + 9^2 = 82 >> 8^2 + 2^2 = 68 >> 6^2 + 8^2 = 100 >> 1^2 + 0^2 + 0^2 = 1
+// We reached 1 meaning that 19 is a Happy Number.
+
+happyOrNot(203) // true
+happyOrNot(11) // false
+happyOrNot(107) // false
