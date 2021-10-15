d8e2c98d619b1d1837ae9d5930912f2b5be6be16
Bebolor <bolorchimeg.m@gmail.com>
2021-10-15 09:44:25 +0200

    daily js challenge

--- n41.js
+++ n41.js
@@ -1,1 +1,18 @@
 //41. Tic Tac Toe
+ticTacToe([
+    ["X", "O", "X"],
+    ["O", "X",  "O"],
+    ["O", "X",  "X"]
+]) // "Winner: X"
+
+ticTacToe([
+    ["O", "O", "O"],
+    ["O", "X", "X"],
+    ["E", "X", "X"]
+]) // "Winner: O"
+
+ticTacToe([
+    ["X", "X", "O"],
+    ["O", "O", "X"],
+    ["X", "X", "O"]
+]) // "Draw. Play again"
