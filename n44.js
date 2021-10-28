45ab6cc16bcc1712afe4037cc04cf981dbcdc53e
Bebolor <bolorchimeg.m@gmail.com>
2021-10-28 20:06:10 +0200

    daily js challenge

--- n44.js
+++ n44.js
@@ -1,7 +1,22 @@
 //44. Spanish conjugation
 
-spanishConj("pasar") // "Yo paso, tú pasas, él pasa, nosotros pasamos, vosotros pasáis, ellos pasan."
+const spanishConj = verb => {
+    const pronouns = ['Yo', 'tú', 'él', 'nosotros', 'vosotros', 'ellos'];
+    const ends = {
+        ar: ['o', 'as', 'a', 'amos', 'áis', 'an'],
+        ir: ['o', 'es', 'e', 'imos', 'ís', 'en'],
+        er: ['o', 'es', 'e', 'emos', 'éis', 'en'],
+    }
+    const form = verb.substr(-2);
+    const stem = verb.substr(0, verb.length - 2);
+    return pronouns.reduce((result, pronoun, index) => {
+    return `${result}${pronoun} ${stem}${ends[form][index]}${
+        index === pronouns.length - 1 ? '.' : ', '}`;
+    }, '');
+}
 
-spanishConj("unir") // "Yo uno, tú unes, él une, nosotros unimos, vosotros unís, ellos unen."
+console.log(spanishConj("pasar")); // "Yo paso, tú pasas, él pasa, nosotros pasamos, vosotros pasáis, ellos pasan."
 
-spanishConj("correr") // "Yo corro, tú corres, él corre, nosotros corremos, vosotros corréis, ellos corren."
+console.log(spanishConj("unir")); // "Yo uno, tú unes, él une, nosotros unimos, vosotros unís, ellos unen."
+
+console.log(spanishConj("correr")); // "Yo corro, tú corres, él corre, nosotros corremos, vosotros corréis, ellos corren."
