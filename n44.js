581e31718c1cdbab948e50d0ec29e2809b978ff9
Bebolor <bolorchimeg.m@gmail.com>
2021-10-28 20:05:29 +0200

    daily js challenge

---
+++ n44.js
@@ -0,0 +1,7 @@
+//44. Spanish conjugation
+
+spanishConj("pasar") // "Yo paso, tú pasas, él pasa, nosotros pasamos, vosotros pasáis, ellos pasan."
+
+spanishConj("unir") // "Yo uno, tú unes, él une, nosotros unimos, vosotros unís, ellos unen."
+
+spanishConj("correr") // "Yo corro, tú corres, él corre, nosotros corremos, vosotros corréis, ellos corren."
